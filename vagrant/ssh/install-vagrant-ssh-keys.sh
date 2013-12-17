60d1d28de74b24dbaa46bff2bde48b42d466434d
Nik <nik@khilnani.org>
2013-12-16 23:02:55 -0500

    Update install-vagrant-ssh-keys.sh

--- vagrant/ssh/install-vagrant-ssh-keys.sh
+++ vagrant/ssh/install-vagrant-ssh-keys.sh
@@ -1,1 +1,7 @@
-download-vagrant-ssh-keys.sh 
+#!/bin/sh -x
+
+curl -o ~/.ssh/id_rsa https://raw.github.com/mitchellh/vagrant/master/keys/vagrant
+curl -O ~/.ssh/id_rsa.pub https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
+
+chmod 600 ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa.pub
