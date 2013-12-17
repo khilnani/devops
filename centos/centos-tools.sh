f820aaa8bef0fb2913f89a354ac8c94d34793b0d
Nik <nik@khilnani.org>
2013-12-16 21:31:41 -0500

    Update centos-tools.sh

--- centos/centos-tools.sh
+++ centos/centos-tools.sh
@@ -1,25 +1,16 @@
 #!/bin/sh -x
 
 ##############################################
-#
+# Install core software
 ##############################################
 
 yum -y groupinstall "Development Tools"
-yum -y install man
-yum -y install ruby
-yum -y install wget
-yum -y install curl
-yum -y install git-core
-yum -y install links lynx
-
+yum -y install man ruby wget curl git-core 
 yum -y install gcc g++ make automake autoconf curl-devel openssl-devel zlib-devel httpd-devel apr-devel apr-util-devel sqlite-devel
-
 yum -y install ruby-rdoc ruby-devel
-
 yum -y install rubygems
 gem update
 gem update --system
-
 yum -y install python-setuptools
 easy_install pip
 
