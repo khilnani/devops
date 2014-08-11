bdff1bccbb5a48359c079e6d7ab336e850ce94e7
Nik <nik@khilnani.org>
2014-08-10 20:44:05 -0400

    Update install-jdk.sh

--- chromebook/install-jdk.sh
+++ chromebook/install-jdk.sh
@@ -3,5 +3,9 @@
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java7-installer
+sudo apt-get install oracle-java7-set-default
 java -version
+
+## Uninstall
+## sudo apt-get remove oracle-java7-installer
 
