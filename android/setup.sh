a137830a2d2b94eed3140f2443c30ee7b7fdb6b4
Nik <nik@khilnani.org>
2014-09-10 19:47:07 -0400

    Update setup.sh

--- android/setup.sh
+++ android/setup.sh
@@ -4,7 +4,9 @@ mkdir ~/bin
 cd ~/bin
 curl -O -k https://raw.githubusercontent.com/khilnani/devops/master/android/py
 chmod 755 ~/bin/py
-echo "export PATH=$PATH:~/bin" >> ~/.bashrc
+echo "export PATH=$PATH:~/bin:/data/data/com.nullient.angoide/files/go/bin" >> ~/.bashrc
+echo "export GOROOT=/data/data/com.nullient.angoide/files/go" >> ~/.bashrc
+echo "export GOPATH=~/sdcard/Go" >> ~/.bashrc
 cd ~/sdcard/Download
 curl -O -k http://android-scripting.googlecode.com/files/sl4a_r6.apk
 curl -O -k https://python-for-android.googlecode.com/files/PythonForAndroid_r5.apk
