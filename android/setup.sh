ec3320dbbcc45ddc9cd9161b2aeb6f9549d168a0
Nik <nik@khilnani.org>
2014-09-10 18:14:44 -0400

    Update setup.sh

--- android/setup.sh
+++ android/setup.sh
@@ -1,4 +1,6 @@
-mkdir ~/bin && cd ~/bin
+#!/bin/system/sh
+mkdir ~/bin
+cd ~/bin
 curl -O -k https://raw.githubusercontent.com/khilnani/devops/master/android/py
 chmod 755 ~/bin/py
 echo "export PATH=$PATH:~/bin" >> ~/.bashrc
