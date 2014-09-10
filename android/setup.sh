62ef444671dfe461bb6e5089a502c110e3a4e068
Nik <nik@khilnani.org>
2014-09-10 18:23:42 -0400

    Update setup.sh

--- android/setup.sh
+++ android/setup.sh
@@ -5,3 +5,6 @@ cd ~/bin
 curl -O -k https://raw.githubusercontent.com/khilnani/devops/master/android/py
 chmod 755 ~/bin/py
 echo "export PATH=$PATH:~/bin" >> ~/.bashrc
+cd ~/sdcard/Download
+curl -O -k http://android-scripting.googlecode.com/files/sl4a_r6.apk
+curl -O -k https://python-for-android.googlecode.com/files/PythonForAndroid_r5.apk
