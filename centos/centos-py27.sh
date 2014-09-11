5be319d30b4f769576eed3c4bd35ffd07a25f45b
khilnani <nik@khilnani.org>
2014-09-11 17:56:02 -0400

    Added python2.7 for centos6.5

---
+++ centos/centos-py27.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd /opt
+wget --no-check-certificate https://www.python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz
+tar xf Python-2.7.6.tar.xz
+cd Python-2.7.6
+./configure --prefix=/usr
+make && make altinstall
+
+cd /opt
+wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
+python2.7 ez_setup.py
+easy_install-2.7 pip
+pip2.7 install virtualenv
+mkdir /opt/py27
+virtualenv /opt/py27
