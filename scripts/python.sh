7747e22229d2d97c139c5c7c2caf0534268e48f6
khilnani <nik@khilnani.org>
2016-10-02 22:00:25 -0400

    updated

--- scripts/python.sh
+++ scripts/python.sh
@@ -1,7 +1,16 @@
-#!/bin/bash -x
+#!/bin/sh
 
+echo "Installing Python libs"
 
 pip install ndg-httpsclient --upgrade
 #pip install pyopenssl ndg-httpsclient pyasn1 --upgrade
 pip install requests[security] --upgrade
-pip install virtualenvwrapper
+
+pip install virtualenvwrapper --upgrade
+
+if grep -q "WORKON_HOME" ~/.bashrc; then
+    echo "virtualenv already in ~/.bashrc"
+else
+    echo 'export WORKON_HOME=$HOME/.virtualenvs' >> ~/.bashrc
+    echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
+fi
