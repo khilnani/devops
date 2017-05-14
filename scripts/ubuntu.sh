4d773ce5eaf8bd72bb0551b698c8d7f70b201461
khilnani <nik@khilnani.org>
2017-05-14 22:53:08 +0000

    updated

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -5,8 +5,8 @@ apt-get -y update
 
 echo '## Install packages'
 apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties --upgrade
-apt-get -y install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev git-core --upgrade
-apt-get -y install software-properties-common python-software-properties python g++ make man curl unzip --upgrade
+apt-get -y install libgdbm-dev libncurses5-dev automake libtool bison ed gawk libc6-dev libffi-dev git-core --upgrade
+apt-get -y install software-properties-common python-software-properties python gcc g++ make man curl unzip --upgrade
 apt-get -y install apache2-utils dnsutils wget --upgrade
 apt-get -y install supervisor nginx --upgrade
 # Jupyter libSM.so.6 error
@@ -24,6 +24,9 @@ apt-get -y install python-setuptools --upgrade
 #apt-get -y install python-pip
 easy_install pip
 easy_install virtualenv
+
+# go
+apt-get install mercurial --upgrade
 
 # aws cli
 pip install awscli --upgrade
