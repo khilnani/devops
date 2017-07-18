2c80396823a7ea29a6bc203da290b52b77869570
khilnani <nik@khilnani.org>
2017-07-17 20:48:54 -0400

    updated

--- windows/virtualbox.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# https://help.ubuntu.com/community/VirtualBox/Installation
-sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list" && wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add - && sudo apt-get update && sudo apt-get install virtualbox-5.0
