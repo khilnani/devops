b4cc31b4131e81982f2a0543ec76d6fe07b61674
Nik <nik@khilnani.org>
2017-07-30 11:44:13 -0400

    Update ruby.sh

--- scripts/ruby.sh
+++ scripts/ruby.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -x
 
-export RVM_VERSION=2.3
+export RVM_VERSION=2.4.1
 
 echo '## Install Ruby'
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -L https://get.rvm.io | bash -s stable
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
+# curl -sSL https://get.rvm.io | bash -s stable
+curl -L https://get.rvm.io | bash -s stable 
 rvm install $RVM_VERSION
 rvm use $RVM_VERSION --default
 ruby -v
