6ce39676f648e1b51990960f2edd6a200684e598
khilnani <nik@khilnani.org>
2018-07-17 01:25:14 -0400

    updated

--- ubuntu/provision-ubuntu.sh
+++ ubuntu/provision-ubuntu.sh
@@ -7,7 +7,6 @@ echo '## Install packages'
 apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties
 apt-get -y install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev git-core
 apt-get -y install software-properties-common python-software-properties python g++ make man mercurial meld curl
-apt-get -y install ubuntu-desktop
 
 echo '## Setup python'
 apt-get -y install python-dev
@@ -15,52 +14,24 @@ apt-get -y install python-setuptools
 easy_install pip
 easy_install virtualenv
 
-echo '## Kernal header files for VirtualBox'
-sudo apt-get install linux-headers-$(uname -r)
+pip install awscli --upgrade
+pip install botocore boto3 --upgrade
+pip install google-cloud-texttospeech --upgrade
+pip install requests==2.6.0
+pip install nltk --upgrade
+pip install newspaper --upgrade
+pip install numpy --upgrade
+pip install sumy --upgrade
 
-echo '## Virtual Box Guest Additions'
-sudo mount /dev/cdrom /mnt
-cd /mnt
-sudo ./VBoxLinuxAdditions.run
+echo '## Setup Node'
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
-echo '## Install Ruby'
-curl -L https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
-echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc
-rvm install 2.3
-rvm use 2.3 --default
-ruby -v
-echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 8.10.0
+npm install -g serverless
 
-echo '## Install Rails'
-gem install rails
-rbenv rehash
-rails -v
-
-echo '## Install NodeJS'
-add-apt-repository -y ppa:chris-lea/node.js
-apt-get -y update
-apt-get -y install nodejs
-
-echo '## Install Chrome'
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - 
-sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-apt-get -y update 
-apt-get -y install google-chrome-stable
-
-echo '## Install Firefox'
-sudo apt-get install firefox
-
-echo '## Cucumber'
-gem install cucumber
-
-echo '## Selenium headless'
-sudo apt-get install xvfb
-mkdir /usr/lib/selenium
-cd /usr/lib/selenium
-wget http://selenium-release.storage.googleapis.com/2.43/selenium-server-standalone-2.43.1.jar
-ln -s selenium-server-standalone-2.43.1.jar selenium-server-standalone.jar
-mkdir -p /var/log/selenium
-chmod a+w /var/log/selenium
-
+#curl -O https://repo.anaconda.com/archive/Anaconda2-5.2.0-Linux-x86_64.sh
+#bash Anaconda3-5.2.0-Linux-x86_64.sh
 
