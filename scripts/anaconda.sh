0d65dccf65f2f1b6499c04c33035f679ff7199aa
Nik Khilnani <nik@khilnani.org>
2019-02-28 22:29:37 -0500

    Update anaconda.sh

--- scripts/anaconda.sh
+++ scripts/anaconda.sh
@@ -3,5 +3,7 @@
 #  https://docs.continuum.io/anaconda/install
 
 echo '## Install Anaconda'
-curl -O http://repo.continuum.io/archive/Anaconda2-$ANACONDA_VERSION-Linux-x86_64.sh
-bash Anaconda2-$ANACONDA_VERSION-Linux-x86_64.sh -b -p $ANACONDA_HOME
+curl -O https://repo.anaconda.com/archive/Anaconda3-$ANACONDA_VERSION-Linux-x86_64.sh
+bash Anaconda3-$ANACONDA_VERSION-Linux-x86_64.sh -b -p $ANACONDA_HOME
+
+conda install -c conda-forge tensorflow
