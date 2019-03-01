694c882ed272f7bc3c571b85044905836c75cccb
Nik Khilnani <nik@khilnani.org>
2019-03-01 07:42:55 -0500

    Update anaconda.sh

--- scripts/anaconda.sh
+++ scripts/anaconda.sh
@@ -6,4 +6,5 @@ echo '## Install Anaconda'
 curl -O https://repo.anaconda.com/archive/Anaconda3-$ANACONDA_VERSION-Linux-x86_64.sh
 bash Anaconda3-$ANACONDA_VERSION-Linux-x86_64.sh -b -p $ANACONDA_HOME
 
-conda install -c conda-forge tensorflow
+conda config --add channels conda-forge
+conda install --yes m2-base
