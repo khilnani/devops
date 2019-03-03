c9c2a5e515b1e3e6aff25c328c282ab5118c698c
Nik Khilnani <nik@khilnani.org>
2019-03-03 15:35:12 -0500

    Create conda-setup.bat

---
+++ anaconda3/conda-setup.bat
@@ -0,0 +1,11 @@
+#!/bin/sh -x
+
+export PATH=/opt/anaconda3/bin:$PATH
+
+call conda config --add channels conda-forge
+call conda update --all
+
+call conda install --yes --file conda-win.tx
+call conda install --yes --file conda.txt
+call conda install --yes --channel "conda-forge" --file conda-forge.txt
+call pip -q install -r pip.txt
