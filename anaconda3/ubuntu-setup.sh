a5dc0740e27fc63a52825bc536a14578524291af
khilnani <nik@khilnani.org>
2019-03-10 16:33:11 +0000

    updated

--- anaconda3/ubuntu-setup.sh
+++ anaconda3/ubuntu-setup.sh
@@ -6,7 +6,7 @@ apt-get -y update
 echo '## Install packages'
 apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties --upgrade
 apt-get -y install libgdbm-dev libncurses5-dev automake libtool bison ed gawk libc6-dev libffi-dev git-core --upgrade
-apt-get -y install software-properties-common python-software-properties python gcc g++ make man curl unzip --upgrade
+apt-get -y install software-properties-common python-software-properties python gcc g++ make man curl unzip zip --upgrade
 apt-get -y install python3-pip python3-dev python3-setuptools --upgrade
 apt-get -y install apache2-utils dnsutils wget --upgrade
 apt-get -y install supervisor nginx --upgrade
