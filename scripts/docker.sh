207bae31bb1681916219d7ad31f77a88c76d6148
Nik Khilnani <nik@khilnani.org>
2019-03-16 15:33:43 -0400

    Update docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,5 +1,18 @@
 #!/bin/bash -x
 
-apt-get -y install docker.io
-apt-get -y install docker-compose
-pip install --upgrade docker-py
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
