c68239c83793a77bb70ed5c346233c225ce99867
Nik Khilnani <nik@khilnani.org>
2019-03-17 22:23:10 -0400

    Update node.sh

--- scripts/node.sh
+++ scripts/node.sh
@@ -2,11 +2,11 @@
 
 # apt-get -y install build-essential 
 # https://github.com/creationix/nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 source ~/.bashrc
 
-nvm install 6.10
-nvm use node 6.10
-nvm alias default 6.10
+nvm install 8.10
+nvm use node 8.10
+nvm alias default 8.10
 
 node -v
